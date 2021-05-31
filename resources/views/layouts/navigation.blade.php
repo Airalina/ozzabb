60c77071bb51e720fad798825e0e94d132d2fc9e
Omar Mrad <mradomar@gmail.com>
2021-05-30 22:25:37 -0300

    Configuración Inicial

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Inicio
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('clientes')">
+                        Clientes
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('proveedores')">
+                        Proveedores
                     </x-nav-link>
                 </div>
             </div>
