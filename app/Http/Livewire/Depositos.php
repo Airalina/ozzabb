7a86fd08ffea47e77ad1f1c38eae900905b3c100
Omar Mrad <mradomar@gmail.com>
2021-09-03 15:27:37 -0300

    hotfix

--- app/Http/Livewire/Depositos.php
+++ app/Http/Livewire/Depositos.php
@@ -36,7 +36,6 @@ class Depositos extends Component
             ->orWhere('description','LIKE','%'.$this->searchmateriales.'%')
             ->orWhere('line_id','LIKE','%'.$this->searchmateriales.'%')
             ->orWhere('usage_id','LIKE','%'.$this->searchmateriales.'%')
-            ->orWhere('replace','LIKE','%'.$this->searchmateriales.'%')
             ->orWhere('stock_min','LIKE','%'.$this->searchmateriales.'%')
             ->orWhere('stock_max','LIKE','%'.$this->searchmateriales.'%')
             ->orWhere('stock','LIKE','%'.$this->searchmateriales.'%')->get();
