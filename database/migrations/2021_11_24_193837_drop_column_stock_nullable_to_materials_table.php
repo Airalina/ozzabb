84d11a550d5487122604d5b3edc3dfead4ce0241
Emiliano1993- <emiliano@codigitar.com>
2021-11-24 16:50:12 -0300

    Modificación en distintas migrations

--- database/migrations/2021_11_24_193837_drop_column_stock_nullable_to_materials_table.php
+++ database/migrations/2021_11_24_193837_drop_column_stock_nullable_to_materials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStockNullableToMaterialsTable extends Migration
+class DropColumnStockNullableToMaterialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddStockNullableToMaterialsTable extends Migration
     public function up()
     {
         Schema::table('materials', function (Blueprint $table) {
-            $table->dropColumn('stock');
+            $table->integer('stock')->nullable();
         });
     }
 
