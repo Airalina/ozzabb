4de6a4f89f58df633b62e3bb91b5a9e1cb4a705c
Airalina <airalinac@gmail.com>
2021-11-26 12:56:26 -0400

    Traduccion login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,14 +17,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" value="Email" />
+                <x-label for="email" value="Correo" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" value="Password" />
+                <x-label for="password" value="Contraseña" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
